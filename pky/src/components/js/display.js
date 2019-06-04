9e8649d8364b785622f4092fe1d880b8136282ab
mochinoko <ys7yob@gmail.com>
2019-06-05 00:05:55 +0300

    modified files

--- pky/src/components/js/display.js
+++ pky/src/components/js/display.js
@@ -1,96 +1,116 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 import Search from "./search.js";
 import Input from "./input.js";
-import Header from './header.js';
-import Cards from './cards.js';
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row"
-import "../style/search.css"
+import Header from "./header.js";
+import Cards from "./cards.js";
+
+import Row from "react-bootstrap/Row";
+import "../style/search.css";
 
 import "../style/display.css";
 
+class Display extends Component {
+  constructor(props) {
+    super(props);
 
-class Display extends Component{
-	constructor(props) {
-		super(props);
+    this.state = {
+      data: {},
+      searchIndex: 0,
+      eventList: [],
+      isloaded: false
+    };
+  }
+  nextResults() {
+    if (this.state.searchIndex > 80) {
+      return console.log("no more items");
+    } else {
+      this.setState({
+        searchIndex: this.state.searchIndex + 9,
+        isloaded: true
+      });
+      this.renderData();
+    }
+  }
+  prevResults() {
+    this.setState({ searchIndex: this.state.searchIndex - 9 });
+    if (this.state.searchIndex < 0) {
+      return console.log("no more items");
+    } else {
+      this.renderData();
+    }
+  }
+  moreDetails() {}
+  renderData() {
+    console.log("old: " + this.state.searchIndex);
+    let data = this.state.data.data.slice(
+      this.state.searchIndex,
+      this.state.searchIndex + 9
+    );
+    this.setState({ eventList: data });
+  }
+  componentDidUpdate() {
+    console.log("new: " + this.state.searchIndex);
+    window.scrollTo(0, 0);
+  }
+  fetchData = () => {
+    fetch("http://localhost:3030/")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          data: data
+        });
+      })
+      .catch(error => console.log(error));
+  };
+  componentDidMount() {
+    this.fetchData();
+  }
+  render() {
+    var events = [];
+    console.log(this.state.eventList);
 
-		this.state = {
-			data:{},
-			searchIndex: 0,
-			eventList:[],
-			isloaded:false
-		};
-	};
-	nextResults(){
-		if(this.state.searchIndex >80){
-			return console.log("no more items");
-		} else{
-			this.setState({searchIndex: this.state.searchIndex+9,
-			isloaded:true})
-			this.renderData();
-		}
-	}
-	prevResults(){
-		this.setState({searchIndex: this.state.searchIndex-9});
-		if(this.state.searchIndex < 0){
-			return console.log("no more items");
-		} else{
-			this.renderData();
-		}
-	}
-	moreDetails(){
-
-	}
-	renderData(){
-		console.log("old: "+this.state.searchIndex);
-		let data = this.state.data.data.slice(this.state.searchIndex, this.state.searchIndex+9);
-		this.setState({eventList: data});
-	}
-	componentDidUpdate(){
-		console.log("new: "+this.state.searchIndex);
-		window.scrollTo(0,0);
-	}
-	fetchData =()=> {
-		fetch("http://localhost:3030/")
-		.then(response => response.json())
-		.then( data => {
-			console.log(data);
-			this.setState({
-				data: data
-			})
-
-			})
-				.catch(error=> console.log(error));
-			}
-componentDidMount(){
-	this.fetchData();
+    if (
+      typeof this.state.eventList !== "undefined" &&
+      this.state.eventList.length > 0
+    ) {
+      events = this.state.eventList.map((event, index) => {
+        return (
+          <div>
+            <Cards key={index} event={event} />
+          </div>
+        );
+      });
+    }
+    return (
+      <div className="body">
+        <Header />
+        <Input id="event" placeholder="please type texts" type="text" />
+        <Search
+          className="searchBttn"
+          label="Search"
+          handleClick={() => this.nextResults()}
+        />
+        <div>
+          <div className="flex-container"> {events}</div>
+          {this.state.isloaded ? (
+            <Row className="justify-content-center">
+              <Search
+                className="bbtn"
+                label="Back"
+                handleClick={() => this.prevResults()}
+              />
+              <Search
+                label="Next"
+                className="bbtn"
+                handleClick={() => this.nextResults()}
+              />
+            </Row>
+          ) : null}
+          <div className="background" />
+        </div>
+      </div>
+    );
+  }
 }
-			render(){
-				var events= [];
-					console.log(this.state.eventList);
-
-				if (typeof this.state.eventList !== 'undefined' && this.state.eventList.length > 0 ){
-					events = this.state.eventList.map( (event, index) =>{
-						return <div><Cards key={index} event={event}/>
-						</div>
-							});
-						}
-						return (
-							<div className="body">
-							<Header />
-							<Input id="event" placeholder="please type texts" type="text"/>
-							<Search  className="searchBttn" label="Search" handleClick={()=>this.nextResults()}/>
-							<div>
-							<div className="flex-container"> {events}</div>
-								{this.state.isloaded?(
-								<Row className="justify-content-center" >
-								<Search className="bbtn"  label="Back"  handleClick={()=>this.prevResults()}/>
-								<Search label="Next" className="bbtn"  handleClick={()=>this.nextResults()}/>
-								</Row>)
-									:null}
-								<div className="background"></div>
-							</div>
-							</div>
-						)}
-					}
-					export default Display;
+export default Display;
--- pky/src/components/js/search.js
+++ pky/src/components/js/search.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import "../style/search.css"
-import {ButtonToolbar} from "react-bootstrap"
-import Button from "react-bootstrap/Button"
+import React, { Component } from "react";
+import "../style/search.css";
+import { ButtonToolbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
-
-class Search extends Component{
-  constructor(props){
+class Search extends Component {
+  constructor(props) {
     super(props);
   }
-render(){
-return(
-<ButtonToolbar>
-<Button className={this.props.className} variant="secondary" size="lg"  onClick={this.props.handleClick} >{this.props.label}
-
-    </Button>
-    </ButtonToolbar>
-
-
-
-
-);
+  render() {
+    return (
+      <ButtonToolbar>
+        <Button
+          className={this.props.className}
+          variant="secondary"
+          size="lg"
+          onClick={this.props.handleClick}
+        >
+          {this.props.label}
+        </Button>
+      </ButtonToolbar>
+    );
+  }
 }
-}
-
-
 
 export default Search;
