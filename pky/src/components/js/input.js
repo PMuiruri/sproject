24f41b92736218546401366bcc5cbff981508cc5
mochinoko <ys7yob@gmail.com>
2019-06-05 10:51:36 +0300

    updated file

--- pky/src/components/js/input.js
+++ pky/src/components/js/input.js
@@ -1,52 +1,21 @@
-import React, { Component } from 'react';
-import "../style/input.css"
-import{ Form} from "react-bootstrap"
-class Input extends Component{
-
-render(){
-return( <div className="input">
-<Form>
-<Form.Control size="lg" type={this.props.type} placeholder={this.props.placeholder} id={this.props.id} />
- 
-</Form>
- </div>
-
-
-);
-
-
-
-
+import React, { Component } from "react";
+import "../style/input.css";
+import { Form } from "react-bootstrap";
+class Input extends Component {
+  render() {
+    return (
+      <div className="input">
+        <Form>
+          <Form.Control
+            size="lg"
+            type={this.props.type}
+            placeholder={this.props.placeholder}
+            id={this.props.id}
+          />
+        </Form>
+      </div>
+    );
+  }
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default Input;
