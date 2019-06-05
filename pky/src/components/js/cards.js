4803ea0942f1295f91eab131a63e0c67637ff2c9
PMuiruri <35254271+PMuiruri@users.noreply.github.com>
2019-06-05 11:54:04 +0300

    added momentjs to convert time

--- pky/src/components/js/cards.js
+++ pky/src/components/js/cards.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {Card} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import "../style/cards.css"
+import moment from 'moment';
 class Cards extends Component{
 
 handleClick=()=> {
@@ -11,7 +12,7 @@ handleClick=()=> {
   render(){
 return (
 <div>
-  <Card className="card" style={{ width: '18rem' }}>
+  <Card className="card" style={{ width: '24rem' }}>
 {this.props.event.description.images.length >0 ?
     (<Card.Img variant="top" src={this.props.event.description.images[0].url}  />)
     : <Card.Img variant="top" src="images/altImage.png" style={{width:'266px', height:'226px'}}/>}
@@ -19,9 +20,9 @@ return (
       <Card.Title>{this.props.event.name.fi}</Card.Title>
       <Card.Text>{this.props.event.description.intro}</Card.Text>
       <Card.Text>{this.props.event.location.address.locality}</Card.Text>
-      <Card.Text>Start Date:{(this.props.event.event_dates.starting_day).split("T")[0]}</Card.Text>
+      <Card.Text> <strong>Start : </strong>{moment.utc(this.props.event.event_dates.starting_day).format("LLLL")}</Card.Text>
       {this.props.event.event_dates.ending_day != null?
-        <Card.Text>End Date:{(this.props.event.event_dates.ending_day).split("T")[0]}</Card.Text>:  null}
+        <Card.Text><strong>End : </strong>{moment.utc(this.props.event.event_dates.ending_day).format("LLLL")}</Card.Text>:  null}
       <Button variant="primary" onClick={this.handleClick}>More Details</Button>
     </Card.Body>
   </Card>
--- pky/src/components/js/eventMap.js
+++ pky/src/components/js/eventMap.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import '../style/event.css';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+
+
+
+L.Icon.Default.mergeOptions({
+
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+
+});
 
 type State = {
   zoom: number,
