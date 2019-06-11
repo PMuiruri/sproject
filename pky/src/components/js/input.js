ad88f179bd3efd0cc7d52874c7347bc7bea33404
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-06-12 00:45:03 +0300

    Added and configured dropdown menu to search for events based on tags

--- pky/src/components/js/input.js
+++ pky/src/components/js/input.js
@@ -1,19 +1,22 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "../style/input.css";
-import { Dropdown, DropdownButton } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
 class Input extends Component {
   render() {
     return (
-      <div className="input">
-        <DropdownButton
-          id="dropdown-basic-button"
-          title="Dropdown button"
-          size="lg"
-        >
-          <Dropdown.Item value="sports">Sports</Dropdown.Item>
-          <Dropdown.Item value="music">Music</Dropdown.Item>
-          <Dropdown.Item value="art">Art</Dropdown.Item>
-        </DropdownButton>
+       <
+      div className = "input" >
+      <Form.Group controlId = "exampleForm.ControlSelect1" >
+      <Form.Control as = "select" onChange={this.props.handleChange}>
+      <option value=""> Search Events< /option>
+     {this.props.options.map((opt) => {
+        return (
+          <option value={opt}> {opt} < /option>
+        );
+      })};
+
+      </Form.Control>
+      </Form.Group>
       </div>
     );
   }
