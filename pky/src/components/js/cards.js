30b31f3b778df21a1431e4c44ea48078c33704ce
PMuiruri <35254271+PMuiruri@users.noreply.github.com>
2019-06-12 12:09:24 +0300

    cleaned up code

--- pky/src/components/js/cards.js
+++ pky/src/components/js/cards.js
@@ -5,30 +5,30 @@ import "../style/cards.css"
 import moment from 'moment';
 class Cards extends Component{
 
-handleClick=()=> {
-	let id= this.props.event.id;
-	this.props.moreDetails(id);
-}
-  render(){
-return (
-<div>
-  <Card className="card" style={{ width: '24rem' }}>
-{this.props.event.description.images.length >0 ?
-    (<Card.Img variant="top" src={this.props.event.description.images[0].url}  />)
-    : <Card.Img variant="top" src="images/altImage.png" style={{width:'266px', height:'226px'}}/>}
-    <Card.Body>
-      <Card.Title>{this.props.event.name.fi}</Card.Title>
-      <Card.Text>{this.props.event.description.intro}</Card.Text>
-      <Card.Text>{this.props.event.location.address.locality}</Card.Text>
-      <Card.Text> <strong>Start : </strong>{moment.utc(this.props.event.event_dates.starting_day).format("LLLL")}</Card.Text>
-      {this.props.event.event_dates.ending_day != null?
-        <Card.Text><strong>End : </strong>{moment.utc(this.props.event.event_dates.ending_day).format("LLLL")}</Card.Text>:  null}
-      <Button variant="primary" onClick={this.handleClick}>More Details</Button>
-    </Card.Body>
-  </Card>
-</div>
-);
+	handleClick=()=> {
+		let id= this.props.event.id;
+		this.props.moreDetails(id);
+	}
+	render(){
+		return (
+			<div>
+			<Card className="card" style={{ width: '24rem' }}>
+			{this.props.event.description.images.length >0 ?
+				(<Card.Img variant="top" src={this.props.event.description.images[0].url}  />)
+				: <Card.Img variant="top" src="images/altImage.png" style={{width:'266px', height:'226px'}}/>}
+				<Card.Body>
+				<Card.Title>{this.props.event.name.fi}</Card.Title>
+				<Card.Text>{this.props.event.description.intro}</Card.Text>
+				<Card.Text>{this.props.event.location.address.locality}</Card.Text>
+				<Card.Text> <strong>Start : </strong>{moment.utc(this.props.event.event_dates.starting_day).format("LLLL")}</Card.Text>
+				{this.props.event.event_dates.ending_day != null?
+					<Card.Text><strong>End : </strong>{moment.utc(this.props.event.event_dates.ending_day).format("LLLL")}</Card.Text>:  null}
+					<Button variant="primary" onClick={this.handleClick}>More Details</Button>
+					</Card.Body>
+					</Card>
+					</div>
+				);
 
-  }
-}
-export default Cards
+			}
+		}
+		export default Cards
--- pky/src/components/js/event.js
+++ pky/src/components/js/event.js
@@ -1,36 +1,42 @@
-import React, { Component } from 'react';
-import {Jumbotron} from 'react-bootstrap';
-import EventMap from './eventMap.js';
-import moment from 'moment';
-
+import React, { Component } from "react";
+import { Jumbotron } from "react-bootstrap";
+import EventMap from "./eventMap.js";
+import moment from "moment";
+import Row from "react-bootstrap/Row";
+import Search from "./search.js";
 
 class Event extends Component {
-
   render() {
     return (
       <div>
-      <Jumbotron>
-      <h1>{this.props.data.name.fi}</h1>
-      <p>
-      	<div dangerouslySetInnerHTML={{__html: this.props.data.description.body}}></div>
-      </p>
-      <p> <strong>Street Address : </strong>
-      {this.props.data.location.address.street_address} {this.props.data.location.address.postal_code} {this.props.data.location.address.locality}
-      </p>
-      <p> <strong>Tags : </strong>
-      {this.props.data.tags[0].name}
-      </p>
-      <p>  <strong>Start : </strong>{moment.utc(this.props.data.event_dates.starting_day).format("LLLL")}</p>
-      {this.props.data.event_dates.ending_day != null?
-        <p><strong>End : </strong>{moment.utc(this.props.data.event_dates.ending_day).format("LLLL")}</p>:  null}
-      </Jumbotron>
-      <div>
-      <EventMap data={this.props.data.location}/>
-      </div>
+        <Jumbotron>
+          <h1>{this.props.data.name.fi}</h1>
+          <p dangerouslySetInnerHTML={{__html: this.props.data.description.body}}/>
+          <p><strong>Street Address : </strong>
+            {this.props.data.location.address.street_address}{" "}
+            {this.props.data.location.address.postal_code}{" "}
+            {this.props.data.location.address.locality}
+          </p>
+          <p><strong>Tags : </strong>
+            {this.props.data.tags[0].name}
+          </p>
+          <p><strong>Start : </strong>
+            {moment.utc(this.props.data.event_dates.starting_day).format("LLLL")}
+          </p>
+          {this.props.data.event_dates.ending_day != null ? (
+            <p><strong>End : </strong>
+              {moment.utc(this.props.data.event_dates.ending_day).format("LLLL")}
+            </p>
+          ) : null}
+        </Jumbotron>
+        <div>
+          <EventMap data={this.props.data.location} />
+          <Row className="justify-content-center">
+          </Row>
+        </div>
       </div>
     );
   }
-
 }
 
 export default Event;
