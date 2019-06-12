249766b6aec189cfa9aef8333f7692bc029f9c6e
PMuiruri <35254271+PMuiruri@users.noreply.github.com>
2019-06-12 12:10:02 +0300

    added first element to search by location

--- pky/src/components/js/input.js
+++ pky/src/components/js/input.js
@@ -2,19 +2,32 @@ import React, {Component} from "react";
 import "../style/input.css";
 import Form from 'react-bootstrap/Form';
 class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+
+    }
+  }
   render() {
     return (
-       <
-      div className = "input" >
+      <div className = "input" >
       <Form.Group controlId = "exampleForm.ControlSelect1" >
       <Form.Control as = "select" onChange={this.props.handleChange}>
-      <option value=""> Search Events< /option>
-     {this.props.options.map((opt) => {
+      <option value=""> Search by Event tags< /option>
+      {this.props.options.map((opt, index) => {
         return (
-          <option value={opt}> {opt} < /option>
+          <option key={index} value={opt}> {opt} < /option>
         );
       })};
 
+      </Form.Control>
+      <Form.Control as = "select" onChange={this.props.handleChange}>
+      <option value=""> Search by Location< /option>
+      return (
+          <option value="Helsinki"> Helsinki < /option>
+          <option value="Espoo"> Espoo < /option>
+          <option value="Vantaa"> Vantaa < /option>
+        );
       </Form.Control>
       </Form.Group>
       </div>
