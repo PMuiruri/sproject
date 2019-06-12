e783789ca594db9e5d0543900f155c9b9c5928ca
mochinoko <ys7yob@gmail.com>
2019-06-12 10:46:35 +0300

    fixed styling

--- pky/src/components/js/input.js
+++ pky/src/components/js/input.js
@@ -1,22 +1,19 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../style/input.css";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 class Input extends Component {
   render() {
     return (
-       <
-      div className = "input" >
-      <Form.Group controlId = "exampleForm.ControlSelect1" >
-      <Form.Control as = "select" onChange={this.props.handleChange}>
-      <option value=""> Search Events< /option>
-     {this.props.options.map((opt) => {
-        return (
-          <option value={opt}> {opt} < /option>
-        );
-      })};
-
-      </Form.Control>
-      </Form.Group>
+      <div className="input">
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Control as="select" onChange={this.props.handleChange}>
+            <option value=""> Search Events</option>
+            {this.props.options.map(opt => {
+              return <option value={opt}> {opt} </option>;
+            })}
+            ;
+          </Form.Control>
+        </Form.Group>
       </div>
     );
   }
