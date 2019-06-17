ff810f6272c95a7b662eaa3c5c5a09966fb1a183
joekaranka14 <48237849+joekaranka14@users.noreply.github.com>
2019-06-17 13:52:49 +0300

    styled the manin select box

--- pky/src/components/js/input.js
+++ pky/src/components/js/input.js
@@ -5,9 +5,9 @@ class Input extends Component {
   render() {
     return (
       <div className="input">
-        <Form.Group controlId="exampleForm.ControlSelect1">
-          <Form.Control as="select" onChange={this.props.handleChange}>
-            <option value=""> Search Events</option>
+        <Form.Group controlId="exampleForm.ControlSelect1" >
+          <Form.Control as="select" onChange={this.props.handleChange} className="event-selector">
+            <option value="">Select an Event</option>
             {this.props.options.map(opt => {
               return <option value={opt}> {opt} </option>;
             })}
