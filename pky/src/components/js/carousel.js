20594de79f0d53cef734afafa541f56fc29786d6
joekaranka14 <48237849+joekaranka14@users.noreply.github.com>
2019-06-17 11:58:31 +0300

    updated the carousel and styling

--- pky/src/components/js/carousel.js
+++ pky/src/components/js/carousel.js
@@ -1,5 +1,9 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel'
+import "../style/carousel.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Carousel from 'react-bootstrap/Carousel';
+import Button from "react-bootstrap/Button";
 
 
 
@@ -26,30 +30,35 @@ class ControlledCarousel extends React.Component {
     const { index, direction } = this.state;
 console.log(this.props.carouselItems);
     return (
-      <Carousel
-        activeIndex={index}
-        direction={direction}
-        onSelect={this.handleSelect}
+      <Container>
+        <Row className="justify-content-between align-items-center vh-60">
+          <div className="col-md-5">
+            <h1 className="text-white">Events Happening Around you!</h1>
+            <p className="text-white">We fetch the web to find you events that are happening around you - particularly in Helsinki, Vantaa and Espoo.</p>
+            <Button className="btn-sm btn-round" value="food" onClick={this.props.handleAll}><i className="fas fa-align-left links-icon"></i> View Featured Events</Button>
+          </div>
+          <div className="col-md-6">
+            <Carousel activeIndex={index} direction={direction} onSelect={this.handleSelect} >
+              {this.props.carouselItems.map((event , index)=>{
+                return(   
+                  <Carousel.Item key={index}>
+                    {event.description.images.length >0 ?
+                      (  <img className="d-block w-100" src={event.description.images[0].url}  alt='events around' /> )
+                      : <img className="d-block w-100" src="images/altImage1.jpg" alt='events happening' />}
+                      <Carousel.Caption>
+                        <h2>{event.name.fi}</h2>
+                        <h3>{(event.event_dates.starting_day).split("T")[0]} </h3>
+                      </Carousel.Caption>
+                  </Carousel.Item>
+                  )
+                }) }
+            </Carousel>
+          </div>
+        </Row>
+      </Container>
 
-      >
-      {this.props.carouselItems.map((event , index)=>{
-        return(   <Carousel.Item key={index}>
-          {event.description.images.length >0 ?
-            (  <img
-                className="d-block w-100"
-                src={event.description.images[0].url}
-                alt=''
-              />)
-      : <img  className="d-block w-100" src="images/altImage1.jpg" alt='' />}
-            <Carousel.Caption>
-              <h2>{event.name.fi}</h2>
-              <h4>{(event.event_dates.starting_day).split("T")[0]} </h4>
-            </Carousel.Caption>
-          </Carousel.Item>)
-
-      }) }
-
-      </Carousel>
+      // container
+      
     );
   }
 }
