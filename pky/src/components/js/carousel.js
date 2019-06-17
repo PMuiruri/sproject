3147d90086a2cb48caa466145537635e9c002400
PMuiruri <35254271+PMuiruri@users.noreply.github.com>
2019-06-17 11:57:57 +0300

    fixed error onbody of description

--- pky/src/components/js/carousel.js
+++ pky/src/components/js/carousel.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel'
-
-
-
 class ControlledCarousel extends React.Component {
   constructor(props, context) {
     super(props, context);
